28cad86fe49bb9c7294a26343564bb2d75380352
Mohammad Iqbal <mohammad.iqbal.2409@gmail.com>
2022-06-27 20:40:41 +0700

    Mencoba react props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Contact from './components/Contact';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      <Contact photo={contacts[0].photo} />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,13 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className='card'>
+            <img className='profile-photo' src={props.photo} alt='Foto Profil'></img>
+        </div>
     )
 }
 
